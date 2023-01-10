58dfef41b25ddb53f2a9422aa5989565de096bbb
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-10 13:19:24 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -3,5 +3,6 @@ let yourArray;
 // assign your array the correct value in the next line
 // your code here!
 // code end
+yourArray = ['one', 2, 'three', true, 5, false];
 
 module.exports = yourArray
